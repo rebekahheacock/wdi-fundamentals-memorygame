b69793a720eff1b232d91e7a5bd69f486d8ae36b
Rebekah Heacock Jones <rebekah.heacock@gmail.com>
2016-07-31 19:52:09 -0400

    improve game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,34 +1,9 @@
-/*
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-if (cardTwo === cardFour) {
-	alert("You found a match!");
-}
-else {
-	alert("Sorry, try again.");
-}
-
-if (cardOne === cardTwo) {
-	alert("You found a match!");
-}
-else {
-	alert("Sorry, try again.");
-}
-
-if (cardThree === cardFour) {
-	alert("You found a match!");
-}
-else {
-	alert("Sorry, try again.");
-}
-*/
-
 var board = document.getElementById('game-board');
 
 var numCards = 4;
+
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 
 
@@ -36,9 +11,31 @@ function createBoard() {
 	for (var i = 0; i < numCards; i++) {
 		var card = document.createElement('div');
 		card.className = 'card';
+		card.setAttribute('data-card', cards[i]);
 		board.appendChild(card);
+		card.addEventListener('click', isTwoCards)
+	}
+}
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cardsInPlay) {
+	if (cardsInPlay[0] === cardsinPlay[1]) {
+		alert("You found a match!");
+		return true;
+	} else {
+		alert("Sorry, try again.");
+		return false;
 	}
 }
 
 createBoard();
+
+
 
